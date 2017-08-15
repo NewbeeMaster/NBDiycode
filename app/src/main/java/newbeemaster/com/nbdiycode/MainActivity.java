d440446fc1730913082dca4f2cef2f20330dc978
fuzhipeng <1149324777@qq.com>
2017-08-15 12:55:39 +0800

    hehe

--- app/src/main/java/newbeemaster/com/nbdiycode/MainActivity.java
+++ app/src/main/java/newbeemaster/com/nbdiycode/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("zone");
         setContentView(R.layout.activity_main);
     }
 }
