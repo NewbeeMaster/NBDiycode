91a2c4853d3ac6eab5f26bc2d48707dfcb94dbe0
wangxiaohui <wangxiaohui@ishinetech.com>
2017-08-15 13:05:20 +0800

    hjkl;

--- app/src/main/java/newbeemaster/com/nbdiycode/MainActivity.java
+++ app/src/main/java/newbeemaster/com/nbdiycode/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         System.out.println("zone");
         System.out.println("zone");
-        System.out.println("zone");
         setContentView(R.layout.activity_main);
     }
 }
